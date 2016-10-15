f032f4a030fd2c11765d2dccf68aef6a94d5a259
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-14 21:54:30 -0400

    Initial Commit

--- WardrobeApplication/Controllers/ClothingTypesController.cs
+++ WardrobeApplication/Controllers/ClothingTypesController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WardrobeApplication.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace WardrobeApplication.Controllers
 {
